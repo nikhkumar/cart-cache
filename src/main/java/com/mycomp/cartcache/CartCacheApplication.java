54bd5200817723756aad25571934126842f0c05f
NIKHILKUMAR <NIKHILKUMAR@nikhkumar@gmail.com>
2019-03-19 12:06:21 +0530

    Initial commit

---
+++ src/main/java/com/mycomp/cartcache/CartCacheApplication.java
@@ -0,0 +1,13 @@
+package com.mycomp.cartcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartCacheApplication.class, args);
+	}
+
+}
